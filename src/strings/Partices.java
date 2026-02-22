ccf099d99500561640df83b83ffdc726a2ed6458
Phanindra Kumar Maroju <phanindrakumar9648@gmail.com>
2026-02-23 02:28:33 +0530

    problems

--- src/strings/Partices.java
+++ src/strings/Partices.java
@@ -14,44 +14,59 @@ public class Partices {
         int num = str.length();
         System.out.println(num);
 
-        int count = (int)IntStream.range(0,str.length()).count();
+        int count = (int) IntStream.range(0, str.length()).count();
         System.out.println(count);
 
         //Count vowels in a string
         long vowelsCount =
-                IntStream.range(0,str.length())
-                        .filter(ch -> "aeiou".indexOf(str.charAt(ch)) !=-1)
+                IntStream.range(0, str.length())
+                        .filter(ch -> "aeiou".indexOf(str.charAt(ch)) != -1)
                         .count();
         System.out.println(vowelsCount);
 
         //Count consonants
-        long CountConstants =IntStream.range(0,str.length()).filter(ch-> "aeiou".indexOf(str.charAt(ch)) ==-1)
+        long CountConstants = IntStream.range(0, str.length()).filter(ch -> "aeiou".indexOf(str.charAt(ch)) == -1)
                 .count();
         System.out.println(CountConstants);
 
-            //Print each character in uppercase using streams
-            IntStream.range(0,str.length()).map(ch -> Character.toUpperCase(str.charAt(ch)))
-                    .forEach(ch -> System.out.print((char) ch));
-            System.out.println();
+        //Print each character in uppercase using streams
+        IntStream.range(0, str.length()).map(ch -> Character.toUpperCase(str.charAt(ch)))
+                .forEach(ch -> System.out.print((char) ch));
+        System.out.println();
 
         //Find first repeating character
-            String repatCh = "programming";
-            Set<Character> seen = new HashSet<>();
+        String repatCh = "programming";
+        Set<Character> seen = new HashSet<>();
         Optional<Character> firstRepeated = repatCh.chars().mapToObj(ch -> (char) ch)
-                    .filter(ch -> !seen.add(ch)).findFirst();
+                .filter(ch -> !seen.add(ch)).findFirst();
         firstRepeated.ifPresent(System.out::println);
 
         //Sreams hands-on CAT, TAC, DON, NOD, TELL count frequency, as cat and tac have same character so consider same.
-        List<String> sameFre = Arrays.asList("CAT", "TAC", "DON", "NOD", "TELL");
-         sameFre.stream().collect(Collectors.groupingBy(ch ->
-                         {
-                             char[] characters = ch.toCharArray();
-                             Arrays.sort(characters);
-                             return new String(characters);
-                         },
-                         Collectors.counting())
-         ).forEach((key, value) -> System.out.println(key + "is value of " + value));
+            List<String> sameFre = Arrays.asList("CAT", "TAC", "DON", "NOD", "TELL");
+        sameFre.stream().collect(Collectors.groupingBy(ch ->
+                        {
+                            char[] characters = ch.toCharArray();
+                            Arrays.sort(characters);
+                            return new String(characters);
+                        },
+                        Collectors.counting())
+        ).forEach((key, value) -> System.out.println(key + '-' + value));
 
+        Map<String,List<String>> val = sameFre.stream().collect(Collectors.groupingBy(ch ->
+                                {
+                                    char[] characters = ch.toCharArray();
+                                    Arrays.sort(characters);
+                                    return new String(characters);
+                                },
+                                Collectors.toList())
+                );
+
+        //Ways of implement the output
+            val.entrySet().stream().forEach(ch -> System.out.println(ch.getValue()));
+
+            val.values().stream().filter(list -> list.size()>1)
+                    .map(list -> list.get(0))
+                    .forEach(ch -> System.out.println(ch +" are Anagaram"));
 
     }
 }

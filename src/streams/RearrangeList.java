241e3daddb61d6a48c39498a6bb135ea923ff59b
Phanindra Kumar Maroju <phanindrakumar9648@gmail.com>
2026-03-02 10:56:49 +0530

    Initial commit
    # Conflicts:
    #	src/streams/RearrangeList.java

--- src/streams/RearrangeList.java
+++ src/streams/RearrangeList.java
@@ -3,6 +3,7 @@ package streams;
 
 import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /*You are given a list representing people where "M" = Male and "F" = Female sit alternately.
 Write a program to rearrange the list so that males and females
@@ -11,6 +12,14 @@ public class RearrangeList {
     public static void main(String[] args) {
         List<Character> input = List.of('M', 'M', 'F', 'F', 'M', 'F');
 
+        List<Character> males = input.stream().filter(c-> c=='M').toList();
+        List<Character> females = input.stream().filter(c-> c=='F').toList();
 
+        List<Character> result = IntStream.range(0, Math.min(males.size(), females.size()))
+                .mapToObj(i-> Stream.of(males.get(i),females.get(i)))
+                .flatMap(s->s)
+                .toList();
+
+        System.out.println(result);
     }
 }
